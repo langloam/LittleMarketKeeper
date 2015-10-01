a8bd77f7dae5f3ef69d6196ac7c71317cbd0c0f6
KsKid <a10038830@gmail.com>
2015-10-01 17:32:56 +0800

    修改優惠頁面
    收藏預計新增
    ExpandableListView

--- app/src/main/java/samples/exoguru/materialtabs/common/Tabs/Tab2.java
+++ app/src/main/java/samples/exoguru/materialtabs/common/Tabs/Tab2.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import samples.exoguru.materialtabs.R;
 
@@ -18,5 +19,15 @@ public class Tab2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.tab_business,container,false);
         return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        SearchView searchView = (SearchView) getView().findViewById(R.id.UI_search);
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setIconifiedByDefault(false);
+        searchView.setQueryHint("請輸入欲搜尋的商圈");
+
     }
 }
