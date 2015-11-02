dbd7ce4159f73cc267d1ef97f085741427640b41
KsKid <a10038830@gmail.com>
2015-11-02 15:52:29 +0800

    修改優惠介面，可從資料庫顯示出來

--- app/src/main/java/samples/exoguru/materialtabs/common/Demo/DemoCoupon2.java
+++ app/src/main/java/samples/exoguru/materialtabs/common/Demo/DemoCoupon2.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import samples.exoguru.materialtabs.R;
 
@@ -19,13 +20,21 @@ public class DemoCoupon2 extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar ActBar = getSupportActionBar();
         ActBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_TITLE);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_demo_coupon2, menu);
-        return true;
+        TextView DemoCoupon1_txb2 = (TextView) findViewById(R.id.DemoCoupon1_txb2);
+
+        DemoCoupon1_txb2.setText("熱情邀約您一起 看見新北 ● 探索無限可能\n" +
+                "「2014新北市商圈嘉年華」活動開跑! 期間推出「新北商圈遊程行程」\n規劃出9條不同遊逛行程，教你怎樣玩最好玩~\n活動期間將舉辦一系列商圈行銷活動及消費好康優惠!" +
+                "矽谷溫泉會館為「2014新北市商圈嘉年華」的特約店家，推出限定好康優惠! \n" +
+                "\n凡貴賓出示 \"2014新北市商圈嘉年華活動手冊\"，即可享 \n" +
+                "\n好康優惠1. 住宿優惠享定價5.5折，並贈送限量獨家設計款人字拖兩雙。 (數量有限，送完為止) \n" +
+                "\n好康優惠2. 泡湯兩小時享優惠價 $999 / 2人。" +
+                "\n* 注意事項：住宿泡湯請事先預約並告知使用好康優惠，服務專線 (02)2218-0101。\n" +
+                "\n" +
+                "● 活動期間：2014/9/28(日) ~ 2014/10/31(五) \n" +
+                "● 活動手冊索取地點：至新北市各商圈、觀光工廠、區公所、旅客服務中心及市府洽公處等，約100處提供索取。\n" +
+                "● 更多詳情請至2014新北市商圈嘉年華活動官網：http://www.newtaipeishopping.tw/");
+
     }
 
     @Override
@@ -37,6 +46,11 @@ public class DemoCoupon2 extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == android.R.id.home) {
+            onBackPressed();
             return true;
         }
 
